c05ab2def24780aaf282c1d0d9ecb157d295c1df
BlackPillow <yfati037@gmail.com>
2020-12-26 09:52:06 +0300

    added item details page

--- src/main/java/com/yahya/growth/stockmanagementsystem/controller/ItemController.java
+++ src/main/java/com/yahya/growth/stockmanagementsystem/controller/ItemController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ItemController {
@@ -16,6 +17,12 @@ public class ItemController {
     public String index(Model model) {
         model.addAttribute("items", itemService.findAll());
         return "index";
+    }
+
+    @GetMapping("/{item_id}")
+    public String itemDetail(@PathVariable(name = "item_id") int itemId, Model model) {
+        model.addAttribute("item", itemService.findById(itemId));
+        return "itemdetail";
     }
 
 }
